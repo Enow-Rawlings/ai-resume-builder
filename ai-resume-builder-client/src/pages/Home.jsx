fbf1b0cd22b433e7a97367cf0bea52a40f05cfe2
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 <eryssyre6@gmail.com>
2026-05-22 07:01:36 +0100

    Update client Home page

--- ai-resume-builder-client/src/pages/Home.jsx
+++ ai-resume-builder-client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Banner from '../components/home/banner'
+// import Banner from '../components/home/banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial'
 import Footer from '../components/home/Footer'
-import CTA from '../components/home/CallToAction'
+// import CTA from '../components/home/CallToAction'
 import CallToAction from '../components/home/CallToAction'
 
 const Home = () => {
